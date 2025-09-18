31d678db83f197fa65268488a1a6061702306430
Tom Mullier <tom.mullier@outlook.fr>
2025-09-18 14:15:24 -0400

    Created file and minimal config for AnalyseVerticle. Need Kafka creation

---
+++ src/main/java/com/aut25/vertx/AnalyseVerticle.java
@@ -0,0 +1,7 @@
+package com.aut25.vertx;
+
+import io.vertx.core.AbstractVerticle;
+
+public class AnalyseVerticle extends AbstractVerticle {
+        
+}
--- src/main/java/com/aut25/vertx/Main.java
+++ src/main/java/com/aut25/vertx/Main.java
@@ -10,11 +10,14 @@ public class Main extends AbstractVerticle {
 
     @Override
     public void start(Promise<Void> startPromise) throws Exception {
+        System.out.println("[ MAIN VERTICLE ] Starting MainVerticle...");
 
-        // Load configuration from file
+        /* ---------------------- Load configuration from file ---------------------- */
         JsonObject config = new JsonObject(
                 new String(Files.readAllBytes(Paths.get("src/main/resources/config.json"))));
-        // Déployer le verticle d'ingestion
+        System.out.println("[ MAIN VERTICLE ] Loaded configuration: " + config.encodePrettily());
+
+        /* ---------------------- Deploy the IngestionVerticle ---------------------- */
         vertx.deployVerticle(new IngestionVerticle(), res -> {
             if (res.succeeded()) {
                 System.out.println("[ MAIN VERTICLE ] IngestionVerticle deployed successfully!");
@@ -23,14 +26,24 @@ public class Main extends AbstractVerticle {
             }
         });
 
-        // Déployer le verticle API
-        vertx.deployVerticle(new ApiVerticle(), res -> {
+        /* ---------------------- Deploy the AnalyseVerticle ---------------------- */
+        vertx.deployVerticle(new AnalyseVerticle(), res -> {
             if (res.succeeded()) {
-                System.out.println("[ MAIN VERTICLE ] ApiVerticle deployed successfully!");
+                System.out.println("[ MAIN VERTICLE ] AnalyseVerticle deployed successfully!");
             } else {
-                System.err.println("[ MAIN VERTICLE ] Failed to deploy ApiVerticle: " + res.cause());
+                System.err.println("[ MAIN VERTICLE ] Failed to deploy AnalyseVerticle: " + res.cause());
             }
         });
+
+        /* ------------------------ Deploy the ApiVerticle ------------------------- */
+        // vertx.deployVerticle(new ApiVerticle(), res -> {
+        // if (res.succeeded()) {
+        // System.out.println("[ MAIN VERTICLE ] ApiVerticle deployed successfully!");
+        // } else {
+        // System.err.println("[ MAIN VERTICLE ] Failed to deploy ApiVerticle: " +
+        // res.cause());
+        // }
+        // });
 
         startPromise.complete(); // Signale que le MainVerticle est prêt
     }
