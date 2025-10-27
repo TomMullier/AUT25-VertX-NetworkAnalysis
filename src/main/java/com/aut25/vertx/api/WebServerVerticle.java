d44aa65f468421123b773e6c7114d007b5a62b2c
Tom Mullier <tom.mullier@outlook.fr>
2025-10-27 12:07:27 -0400

    Added main web route

--- src/main/java/com/aut25/vertx/api/WebServerVerticle.java
+++ src/main/java/com/aut25/vertx/api/WebServerVerticle.java
@@ -1,14 +1,19 @@
 package com.aut25.vertx.api;
 
+import io.netty.handler.codec.http.cors.CorsHandler;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.json.JsonObject;
+
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.aut25.vertx.utils.Colors;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.*;
+import io.vertx.core.http.HttpMethod;
 
 public class WebServerVerticle extends AbstractVerticle {
 
@@ -27,15 +32,23 @@ public class WebServerVerticle extends AbstractVerticle {
                                         + e.getMessage());
                         return;
                 }
+
                 int port = config.getInteger("http.port", 8888);
                 logger.info(Colors.MAGENTA + "[ WEBSERVER VERTICLE ]            Starting HTTP server on port " + port
                                 + Colors.RESET);
 
+                Router router = Router.router(vertx);
+
+                router.route("/").handler(StaticHandler.create("web").setCachingEnabled(false));
+
+                Router apiRouter = Router.router(vertx);
+                // apiRouter.get("/flows").handler(this::getFlows);
+                // apiRouter.get("/stats").handler(this::getStats);
+                router.mountSubRouter("/api", apiRouter);
+
                 HttpServer server = vertx.createHttpServer();
 
-                server.requestHandler(req -> {
-                        req.response().end("Vert.x API is running!");
-                }).listen(port, ar -> {
+                server.requestHandler(router).listen(port, ar -> {
                         if (ar.succeeded()) {
                                 logger.info(Colors.MAGENTA
                                                 + "[ WEBSERVER VERTICLE ]            HTTP server started on port "
