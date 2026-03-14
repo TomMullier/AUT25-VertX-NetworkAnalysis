26787128888d5e2942cea90ea47e86311abb89c9
root <root@imagin051348.ens.ad.etsmtl.ca>
2026-03-14 17:20:04 -0400

    Stop tracking build/output & env directories; update .gitignore

--- bin/_tests/benchmark/02_phase/misc/flowmeter_gen.py
+++
@@ -1,7 +0,0 @@
-from flowmeter.flowmeter import Flowmeter
-import pandas as pd
-
-feature_gen = Flowmeter("../01_phase/pcap/reference.pcap")
-df = feature_gen.build_feature_dataframe()
-
-df.to_csv("flowmeter_flowmeter.csv", index=False)
